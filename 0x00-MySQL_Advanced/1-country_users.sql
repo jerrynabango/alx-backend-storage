3ad0a7fe1a66aae45b8c911e5d7194eded81058d
Jerry Nabango <jerrynabango9@gmail.com>
2024-01-18 09:11:24 +0300

    creates users table

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 --  script that ranks country origins of bands, ordered by the number of (non-unique) fans
 
-CREATE TABLE IF NOT EXISTS bands (
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL UNIQUE,
