1c3162cd1cb25b2f0129741414e8994fe6deffe9
Jerry Nabango <jerrynabango9@gmail.com>
2024-01-22 22:28:07 +0300

    log stats

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
-"""Log stats - new version"""
-
+"""
+A script that provides some stats about Nginx logs stored in MongoDB
+"""
 from pymongo import MongoClient
 
 
 def log_stat() -> None:
     """
-    Improve 12-log_stats.py by adding the top 10 of the most present
-    IPs in the collection nginx of the database logs:
+    Provides some stats about Nginx logs stored in MongoDB
     """
-    statas = ""
+    stats = ""
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    statas += "{} logs\nMethods:\n".format(nginx_collection.count_documents({}))
-    for method in methods:
-        method_count = nginx_collection.count_documents({"methods": method})
-        statas += "\tmethod {}: {}\n".format(method, method_count)
-    statas += "{} status check".format(
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    stats += "{} logs\nMethods:\n".format(nginx_collection.count_documents({}))
+    for m in method:
+        method_count = nginx_collection.count_documents({"method": m})
+        stats += '\tmethod {}: {}\n'.format(m, method_count)
+    stats += "{} status check".format(
         nginx_collection.count_documents({"path": "/status"}))
-    statas += "\nIPs:\n"
-    ips = nginx_collection.aggregate([
-        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-        {"$sort": {"count": -1}}, {"$limit": 10}])
-    for ip in ips:
-        statas += "\t{}: {}\n".format(ip.get("_id"), ip.get("count"))
-    print(statas, end='')
+    stats += '\nIPs:\n'
+    top_ips = nginx_collection.aggregate([
+        {'$group': {'_id': '$ip', 'count': {'$sum': 1}}},
+        {'$sort': {'count': -1}}, {'$limit': 10}])
+    for ip in top_ips:
+        stats += '\t{}: {}\n'.format(ip.get('_id'), ip.get('count'))
+    print(stats, end='')
 
 
 if __name__ == '__main__':
