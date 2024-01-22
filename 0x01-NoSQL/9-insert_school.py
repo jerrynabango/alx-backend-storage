8281bcd31aa2daac2dfaff264accd176eb5ac906
Jerry Nabango <jerrynabango9@gmail.com>
2024-01-22 20:44:15 +0300

    insert doc

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Insert a document in Python"""
 
-import pymongo
+from typing import Mapping
+from pymongo import MongoClient
 
 
-def insert_school(mongo_collection, **kwargs):
+def insert_school(mongo_collection: MongoClient,
+                  **kwargs: Mapping[str, str]) -> str:
     """inserts a new document in a collection based on kwargs"""
     return mongo_collection.insert_one(kwargs).inserted_id
